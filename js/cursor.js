1be123c514a913eaaa23e7d1429c68b570a599f5
jing <jingtang1927@gmail.com>
2024-11-14 15:54:10 +0800

    map done

---
+++ js/cursor.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const customCursor = document.createElement('div');
+    customCursor.classList.add('custom-cursor');
+    document.body.appendChild(customCursor);
+
+    document.addEventListener('mousemove', (e) => {
+        customCursor.style.left = `${e.clientX}px`;
+        customCursor.style.top = `${e.clientY}px`;
+    });
+});
