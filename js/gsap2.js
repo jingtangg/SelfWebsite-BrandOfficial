71f22d5f5d2a776b9c22d27022751ba1d2a83842
jing <jingtang1927@gmail.com>
2024-12-09 05:13:02 +0800

    微調

--- js/gsap2.js
+++ js/gsap2.js
@@ -4,6 +4,6 @@ gsap.registerPlugin();
 // 動畫邏輯
 gsap.fromTo(
     ".text-title", // 選取目標
-    { y: "100%", opacity: 0 }, // 初始狀態
+    { y: "50%", opacity: 0 }, // 初始狀態
     { y: "0%", opacity: 1, duration: 2, ease: "power2.out" } // 終止狀態
 );
